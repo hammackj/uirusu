b4c1dc3f189854765538cd5b0179a3c45d901a91
Jacob Hammack <jacob.hammack@hammackj.com>
2013-06-14 00:14:55 -0500

    Changed domain name

--- lib/uirusu/cli/application.rb
+++ lib/uirusu/cli/application.rb
@@ -1,20 +1,20 @@
 # Copyright (c) 2012-2013 Arxopia LLC.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
-# Redistributions of source code must retain the above copyright notice, 
+#
+# Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
 #
-# Redistributions in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation 
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
-# Neither the name of the project's author nor the names of its contributors 
-# may be used to endorse or promote products derived from this software 
+# Neither the name of the project's author nor the names of its contributors
+# may be used to endorse or promote products derived from this software
 # without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,7 +29,7 @@
 module Uirusu
 	module CLI
 		class Application
-		
+
 			# Creates a new instance of the [Application] class
 			#
 			def initialize
@@ -40,7 +40,7 @@ module Uirusu
 				@sites = Array.new
 				@uploads = Array.new
 			end
-		
+
 			# Parses the command the line options and returns the parsed options hash
 			#
 			# @return [Hash] of the parsed options
@@ -50,14 +50,14 @@ module Uirusu
 					@options['verbose'] = false
 					@options['rescan']  = false
 					@options[:timeout]  = 25
-				
+
 					opt = OptionParser.new do |opt|
-						opt.banner = "#{APP_NAME} v#{VERSION}\nJacob Hammack\nhttp://www.hammackj.com\n\n"
+						opt.banner = "#{APP_NAME} v#{VERSION}\nJacob Hammack\nhttp://www.arxopia.com\n\n"
 						opt.banner << "Usage: #{APP_NAME} <options>"
 						opt.separator('')
 						opt.separator('File Options')
-				
-						opt.on('-h HASH', '--search-hash HASH', 'Searches a single hash on virustotal.com') do |hash| 
+
+						opt.on('-h HASH', '--search-hash HASH', 'Searches a single hash on virustotal.com') do |hash|
 							@hashes.push(hash)
 						end
 
@@ -74,7 +74,7 @@ module Uirusu
 								puts "[!] #{file} does not exist, please check your input!\n"
 							end
 						end
-					
+
 						opt.on('-u FILE', '--upload-file FILE', 'Uploads a file to virustotal.com for analysis') do |file|
 							if File.exists?(file)
 								puts "[+] Adding file #{file}" if @options['verbose']
@@ -86,11 +86,11 @@ module Uirusu
 
 						opt.separator('')
 						opt.separator("Url Options")
-						
-						opt.on('-s SITE', '--search-site SITE', 'Searches for a single url on virustotal.com') { |site| 
+
+						opt.on('-s SITE', '--search-site SITE', 'Searches for a single url on virustotal.com') { |site|
 							@sites.push(site)
 						}
-												
+
 						opt.separator('')
 						opt.separator('Output Options')
 
@@ -101,11 +101,11 @@ module Uirusu
 						opt.on('-x', '--xml-output', 'Print results as xml to stdout') do
 							@options['output'] = :xml
 						end
-			
+
 						opt.on('-y', '--yaml-output', 'Print results as yaml to stdout') do
 							@options['output'] = :yaml
 						end
-					
+
 						opt.on('--stdout-output', 'Print results as normal text line to stdout, this is default') do
 							@options['output'] = :stdout
 						end
@@ -113,10 +113,10 @@ module Uirusu
 						opt.separator ''
 						opt.separator 'Advanced Options'
 
-						opt.on('-c', '--create-config', 'Creates a skeleton config file to use') do					
+						opt.on('-c', '--create-config', 'Creates a skeleton config file to use') do
 							if File.exists?(File.expand_path(CONFIG_FILE)) == false
-								File.open(File.expand_path(CONFIG_FILE), 'w+') do |f| 
-									f.write("virustotal: \n  api-key: \n  timeout: 25\n\n") 
+								File.open(File.expand_path(CONFIG_FILE), 'w+') do |f|
+									f.write("virustotal: \n  api-key: \n  timeout: 25\n\n")
 								end
 
 								puts "[*] An empty #{File.expand_path(CONFIG_FILE)} has been created. Please edit and fill in the correct values."
@@ -134,10 +134,10 @@ module Uirusu
 						opt.on('--[no-]verbose', 'Print verbose information') do |v|
 							@options['verbose'] = v
 						end
-				
+
 						opt.separator ''
 						opt.separator 'Other Options'
-				
+
 						opt.on('-v', '--version', 'Shows application version information') do
 							puts "#{APP_NAME} - #{VERSION}"
 							exit
@@ -146,21 +146,21 @@ module Uirusu
 						opt.on_tail('-?', '--help', 'Show this message') { |help|
 							puts opt.to_s + "\n"
 							exit
-						} 
+						}
 					end
-							
-				  if ARGV.length != 0 
+
+				  if ARGV.length != 0
 				    opt.parse!
 				  else
 				    puts opt.to_s + "\n"
 					  exit
-					end		
+					end
 				rescue OptionParser::MissingArgument => m
 					puts opt.to_s + "\n"
 					exit
 				end
 			end
-			
+
 			# Loads the .uirusu config file for the api key
 			#
 			def load_config
@@ -173,7 +173,7 @@ module Uirusu
 
 				@options[:timeout] = @config['virustotal']['timeout'] if @config['virustotal']['timeout'] != nil
 			end
-			
+
 			# Submits a file/url and waits for analysis to be complete and returns the results.
 			#
 			# @param mod
@@ -183,7 +183,7 @@ module Uirusu
 			def scan_and_wait(mod, resource, attempts)
 				method = nil
 				retries = attempts
-				
+
 				if mod.name == "Uirusu::VTFile"
 					STDERR.puts "[*] Attempting to rescan #{resource}" if  @options['verbose']
 					method = @options['rescan'] ? mod.method(:rescan_file) : mod.method(:scan_file)
@@ -193,7 +193,7 @@ module Uirusu
 				end
 
 				begin
-					result = method.call(@config['virustotal']['api-key'], resource)					
+					result = method.call(@config['virustotal']['api-key'], resource)
 				rescue => e
 					if @options['rescan']
 						STDERR.puts "[!] An error has occurred with the rescan request.  Retrying 60 seconds up #{retries} retries: #{e.message}\n" if  @options['verbose']
@@ -207,7 +207,7 @@ module Uirusu
 						retries = retries - 1
 					end
 				end
-				
+
 				begin
 
 					# Convert all single result replies to an array.  This is because
@@ -219,13 +219,13 @@ module Uirusu
 						if result['response_code'] == 1
 							STDERR.puts "[*] Attempting to parse the results for: #{result['resource']}" if @options['verbose']
 							results = mod.query_report(@config['virustotal']['api-key'], result['resource'])
-							
+
 							while results['response_code'] != 1
 								STDERR.puts "[*] File has not been analyized yet, waiting 60 seconds to try again" if  @options['verbose']
-								sleep 60				
+								sleep 60
 								results = mod.query_report(@config['virustotal']['api-key'], result['resource'])
 							end
-						
+
 							[result['resource'], results]
 							#return [result['resource'], results]
 
@@ -238,11 +238,11 @@ module Uirusu
 							next
 
 						elsif result['response_code'] == -2
-							STDERR.puts "[!] Virustotal limits exceeded, ***do not edit the timeout values.***" 
+							STDERR.puts "[!] Virustotal limits exceeded, ***do not edit the timeout values.***"
 							exit(1)
 						else
 							nil
-						end	
+						end
 					end
 				rescue => e
 					STDERR.puts "[!] An error has occurred retrieving the report. Retrying 60 seconds up #{retries} retries. #{e.message}\n" if  @options['verbose']
@@ -250,16 +250,16 @@ module Uirusu
 						sleep 60
 						retry
 						retries = retries - 1
-					end				
+					end
 				end
 			end
-			
+
 			#
 			#
 			def main(args)
 				parse_options(args)
 				load_config
-				
+
 				if @options['output'] == :stdout
 					output_method = :to_stdout
 				elsif @options['output'] == :json
@@ -283,7 +283,7 @@ module Uirusu
 							@hashes.push hash
 						end
 					end
-				end		
+				end
 
 				if @hashes != nil
 					@hashes.each_with_index do |hash, index|
@@ -311,7 +311,7 @@ module Uirusu
 				if @uploads != nil
 					@uploads.each_with_index do |upload, index|
 						results = scan_and_wait(Uirusu::VTFile, upload, 5)
-						result = Uirusu::VTResult.new(results[0], results[1])						
+						result = Uirusu::VTResult.new(results[0], results[1])
 						print result.send output_method if result != nil
 						sleep @options[:timeout] if index != @uploads.length - 1
 					end
